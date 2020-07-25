f3f00605cea0dc0dd4cbd9737e02ed65a2ed7eb6
Eduard Laryushkin <ceo@edlar.ru>
2020-07-25 17:15:04 +0300

    Initial Commit

---
+++ PaletteSwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PaletteSwiftUIDemo
+//
+//  Created by Eduard Laryushkin on 25.07.2020.
+//  Copyright © 2020 edlar.ru. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
